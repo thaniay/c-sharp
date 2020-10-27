838cc9a1c0bb71ce8a437fc47277c8ae40e486cd
Thania Yanez <yorelyyanezguevara@gmail.com>
2020-10-26 22:34:21 -0600

    agregando tarea 2

---
+++ Ejercicios/tarea-2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace inventario
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            string opcion = "";
+            Inventario inventario = new Inventario();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Sistema de Inventario");
+                Console.WriteLine("*********************");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Productos");
+                Console.WriteLine("2 - Ingreso de Inventario");
+                Console.WriteLine("3 - Salida de Inventario");
+                Console.WriteLine("4 - Ajuste Positivo");
+                Console.WriteLine("5 - Ajuste Negativo");
+                Console.WriteLine("0 - Salir");
+                Console.WriteLine();
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1": 
+                        inventario.listarProductos();
+                        break;
+                    case "2":
+                        inventario.ingresoDeInventario();
+                        break;
+                    case "3":
+                        inventario.salidaDeInventario();
+                        break; 
+                    case "4":
+                        inventario.AjustePositivo();
+                        break;
+                    case "5":
+                        inventario.AjusteNegativo();
+                        break;                        
+                    default:
+                    break;
+                }
+
+                if (opcion == "0") {
+                    break;
+                }
+            }            
+        }
+    }
+}
